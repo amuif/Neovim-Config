bc947be60c7b38058b076ac9588483bcfc53d300
amuif <amudihayat@gmail.com>
2024-12-07 00:02:09 +0300

    neovim config V2.0

---
+++ lua/plugins/lsp-config.lua
@@ -0,0 +1,55 @@
+return {
+
+	{
+		"williamboman/mason.nvim",
+		config = function()
+			require("mason").setup()
+		end,
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		lazy=false,
+			opts = { auto_install = true},
+	--	config = function()
+	--		require("mason-lspconfig").setup({
+	--			ensure_installed = {
+	--				"lua_ls",
+	--				"eslint",
+	--				"tailwindcss",
+	--				"ts_ls",
+	--				"html",
+	--				"jsonls",
+	--				"gopls",
+	--				"pyright",
+	--							},
+	--		})
+	--	end,
+	},
+
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			local capabilities = require('cmp_nvim_lsp').default_capabilities()
+			local lspconfig = require("lspconfig")
+			-- Configure LSP servers
+			lspconfig.lua_ls.setup({
+			capabilities = capabilities
+			})
+			lspconfig.eslint.setup({
+			capabilities = capabilities
+			})
+			lspconfig.tailwindcss.setup({
+			capabilities = capabilities})
+			lspconfig.ts_ls.setup({
+			capabilities = capabilities})
+			lspconfig.html.setup({capabilities = capabilities})
+			lspconfig.jsonls.setup({capabilities = capabilities})
+			lspconfig.gopls.setup({capabilities = capabilities})
+			lspconfig.pyright.setup({capabilities = capabilities})
+			-- Keymaps for LSP functionality
+			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+			vim.keymap.set({ "n" }, "<leader>ca", vim.lsp.buf.code_action, {})
+		end,
+	},
+}
