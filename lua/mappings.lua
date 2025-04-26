22d00e729291604bfd1178f353a1b16bbebfcd7e
amuif <amudihayat@gmail.com>
2025-04-26 12:51:39 +0300

    added lspsaga

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -7,4 +7,8 @@ local map = vim.keymap.set
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
+vim.keymap.set({ "n", "v" }, "<leader>ca", function()
+  require("lspsaga.codeaction"):code_action()
+end, { desc = "LSPSaga Code Action" })
+
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -66,12 +66,58 @@ return {
       },
     },
   },
+  -- to make a great ui
   {
-    "nvimtools/none-ls.nvim",
-    event = "VeryLazy",
-    opts = function()
-      return require "configs.null-ls"
+    "nvimdev/lspsaga.nvim",
+    event = "LspAttach",
+    config = function()
+    require("lspsaga").setup({
+  lightbulb = {
+    enable = false,  -- disables the bulb icon
+  },
+  symbol_in_winbar = {
+    enable = false,  -- disables the breadcrumb in the winbar
+  },
+  ui = {
+    -- Define how the saga icons should look
+    theme = "round",
+    -- Customize border for floating windows
+    border = "rounded",
+    -- Show lines for hover window
+    winblend = 0,
+    expand = "",
+    collapse = "",
+  },
+
+  code_action = {
+    enable = true,
+    keys = {
+      -- Mapping to trigger code actions
+      quit = "q",
+      exec = "<CR>",  -- Enter key to execute the action
+    },
+  },
+  rename = {
+    enable = true,
+    keys = {
+      quit = "<Esc>",  -- Mapping to exit the rename prompt
+      exec = "<CR>",   -- Enter key to apply the rename
+    },
+  },
+  diagnostic = {
+    enable = true,
+    keys = {
+      -- Mapping to jump to next/prev diagnostic
+      next = "n",
+      prev = "p",
+    },
+  },
+})
     end,
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter",
+      "nvim-tree/nvim-web-devicons",
+    },
   },
   {
     "windwp/nvim-ts-autotag",
