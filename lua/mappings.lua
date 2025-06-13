22806187e04d56c1a2eac09b6879a84ca4454937
amuif <amudihayat@gmail.com>
2025-06-13 21:10:13 +0300

    added tab managment

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -16,8 +16,8 @@ map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
 -- ~/.config/nvim/init.lua or ~/.config/nvim/lua/custom/mappings.lua
 local map = vim.api.nvim_set_keymap
 
--- makes jk emitate escape 
-vim.api.nvim_set_keymap("i","jk","<ESC>",{noremap=false})
+-- makes jk emitate escape
+vim.api.nvim_set_keymap("i", "jk", "<ESC>", { noremap = false })
 -- Show diagnostic float with 'q' to close
 vim.keymap.set("n", "<leader>d", function()
   vim.diagnostic.open_float(nil, {
@@ -27,10 +27,34 @@ vim.keymap.set("n", "<leader>d", function()
   })
 
   -- Automatically map 'q' to close the float if it's a diagnostic window
-  vim.cmd([[
+  vim.cmd [[
     augroup DiagnosticFloat
       autocmd!
       autocmd BufWinEnter * if &buftype ==# 'nofile' | nnoremap <buffer> q :close<CR> | endif
     augroup END
-  ]])
-end,{ desc = "Show line diagnostics" })
+  ]]
+end, { desc = "Show line diagnostics" })
+
+--handling tabs
+-- New tab
+map("n", "<C-t>", ":tabnew<CR>", { desc = "New tab", noremap = true })
+map("i", "<C-t>", "<Esc>:tabnew<CR>", { desc = "New tab", noremap = true })
+map("v", "<C-t>", "<Esc>:tabnew<CR>", { desc = "New tab", noremap = true })
+
+-- Close tab
+map("n", "<C-q>", ":tabclose<CR>", { desc = "Close tab", noremap = true })
+map("i", "<C-q>", "<Esc>:tabclose<CR>", { desc = "Close tab", noremap = true })
+map("v", "<C-q>", "<Esc>:tabclose<CR>", { desc = "Close tab", noremap = true })
+
+-- Next tab
+map("n", "<leader><Right>", ":tabnext<CR>", { desc = "Next tab", noremap = true })
+map("i", "<leader><Right>", "<Esc>:tabnext<CR>", { desc = "Next tab", noremap = true })
+map("v", "<leader><Right>", "<Esc>:tabnext<CR>", { desc = "Next tab", noremap = true })
+
+-- Previous tab
+map("n", "<leader><Left>", ":tabprevious<CR>", { desc = "Previous tab", noremap = true })
+map("i", "<leader><Left>", "<Esc>:tabprevious<CR>", { desc = "Previous tab", noremap = true })
+map("v", "<leader><Left>", "<Esc>:tabprevious<CR>", { desc = "Previous tab", noremap = true })
+
+-- Command mode shortcut
+map("n", ";", ":", { desc = "CMD enter command mode" })
--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -1,9 +1,7 @@
 return {
   {
     "lewis6991/gitsigns.nvim",
-    version = "v1.0.1",  -- replace with the tag, e.g., "v1.14.0"
-    -- or use commit hash:
-    -- commit = "abcdef1234567890abcdef1234567890abcdef12",
+    version = "v1.0.0",
     config = function()
       require("gitsigns").setup()
     end,
